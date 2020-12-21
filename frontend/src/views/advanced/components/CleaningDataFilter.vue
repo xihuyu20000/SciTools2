1d323ae3cf6dee9e0a8fec2e37e444d2dcf61ba3
xihuyu20000 <377486624@qq.com>
2020-12-22 05:01:44 +0800

    高级图表——过滤器

---
+++ frontend/src/views/advanced/components/CleaningDataFilter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    aaaaaaaaa
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      drawerVisible: false
+    }
+  },
+  mounted() {}
+}
+</script>
+
+<style></style>
---
+++ frontend/src/views/advanced/components/CleaningFilterExpression.vue
@@ -0,0 +1,11 @@
+<template
+  ><div>{{ field.title }}</div>
+</template>
+
+<script>
+export default {
+  props: ['field']
+}
+</script>
+
+<style></style>
